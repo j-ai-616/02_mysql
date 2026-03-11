791b3dcfe98d1e18710c127036535e7828ee0d4f
j-ai-616 <yjson616@gmail.com>
2026-03-11 15:22:44 +0900

    Feat: DCL 추가

--- 16_dcl.sql
+++ 16_dcl.sql
@@ -6,6 +6,38 @@
 
 -- 이 스크립트는 권한이 있는 계정(root)으로 실행한다.
 
--- 1. 계정 생성
+--  계정 생성
 CREATE USER 'user01'@'%' IDENTIFIED BY 'pass01';
--- DROP USER IF EXISTS 'user01'@'%';
+-- 계정 삭제
+DROP USER IF EXISTS 'user01'@'%';
+
+-- 권한 확인
+SHOW GRANTS FOR 'user01'@'%';  -- 접속할 수는 있지만 특정 권한은 없는 상태
+
+-- 권한 부여
+-- menudb 데이터베이스의 모든 객체를 조회할 수 있는 권한을 user01에게 부여
+GRANT SELECT ON menudb.* TO 'user01'@'%';
+
+-- 권한 확인
+SHOW GRANTS FOR 'user01'@'%';
+
+-- 여러 권한 한 번에 부여
+-- "메뉴 테이블"에 대한 삽입, 수정 권한을 user01 계정에 부여 (세분화된 권한 부여 가능)
+GRANT INSERT, UPDATE ON menudb.tbl_menu TO 'user01'@'%';
+SHOW GRANTS FOR 'user01'@'%';
+
+-- 권한 회수
+-- 메뉴 테이블에 대한 수정 권한을 user01 계정에게서 회수
+REVOKE UPDATE ON menudb.tbl_menu FROM 'user01'@'%';
+SHOW GRANTS FOR 'user01'@'%';
+
+-- ALL PRIVILIGES : 모든 권한
+-- 모든 권한 회수
+REVOKE ALL PRIVILEGES ON menudb.* FROM 'user01'@'%';
+REVOKE ALL PRIVILEGES ON menudb.tbl_menu FROM 'user01'@'%';
+SHOW GRANTS FOR 'user01'@'%';
+
+-- user01로 접속해서 테스트
+SELECT * FROM tbl_menu;
+INSERT INTO tbl_menu VALUES (NULL, '메뉴명', 1000, 4, 'Y');  -- 오로지 SELECT 권한만 가지고 있다.
+INSERT INTO tbl_category VALUES (15, '이름', 1);
